d7763f9ee1116c6f589c15f561c82195a960c4e3
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-05-28 12:45:00 +1200

    keybinds

--- Project/Scripts/Attached/hand.gd
+++ Project/Scripts/Attached/hand.gd
@@ -5,7 +5,7 @@ const weapon_damage: int = 20
 
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("left_click"):
 		weapon.disabled = false
 	elif weapon.disabled == false:
 		weapon.disabled = true
@@ -13,7 +13,6 @@ func _process(_delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
-		print("step1")
 		@warning_ignore("narrowing_conversion")
 		var new_damage: int = weapon_damage * randf_range(0.8,1.2)
 		SignalManager.enemy_take_damage.emit(new_damage)
--- Project/Scripts/Attached/player.gd
+++ Project/Scripts/Attached/player.gd
@@ -9,12 +9,12 @@ func _ready() -> void:
 	SignalManager.player_take_damage.connect(take_damage)
 
 func _physics_process(_delta: float) -> void:
-	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_x := Input.get_axis("move_left", "move_right")
 	if direction_x:
 		velocity.x = direction_x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, slipperiness_factor)
-	var direction_y := Input.get_axis("ui_up", "ui_down")
+	var direction_y := Input.get_axis("move_forward", "move_backward")
 	if direction_y:
 		velocity.y = direction_y * speed
 	else:
