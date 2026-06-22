69b73ce92d8b28d7faf17d1af7e626906d89918c
Isaac Choi <23332@burnside.school.nz>
2026-06-23 10:17:52 +1200

    tidied up files

---
+++ Project/isaac designs/isaac assets/scripts/transition.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	# Check if the colliding body is the player
+	if body.name == "player": # Or use a class_name like: if body is Player:
+		get_tree().change_scene_to_file.call_deferred("res://Levels/homebase.tscn")
