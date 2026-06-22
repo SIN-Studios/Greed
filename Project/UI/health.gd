7424e632d9c4b3feacbf3eadfa741aa861fe2a5f
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-06-22 18:42:47 +1200

    fixed health fx bug when walking into a new scene

--- Project/UI/health.gd
+++ Project/UI/health.gd
@@ -5,10 +5,11 @@ var old_health: int
 
 func _process(_delta: float) -> void:
 	if not player:
+		player = get_tree().get_first_node_in_group("player")
 		return
 
-	$health_indicator.self_modulate.a8 = (100 - float(player.health)) * 2.55
+	$health_indicator.modulate.a8 = (100 - float(player.health)) * 2.55
 	if player.time_till_regen > 2:
-		$health_indicator.self_modulate.r8 = 255
+		$health_indicator.modulate.r8 = 255
 	else:
-		$health_indicator.self_modulate.r8 = lerp($health_indicator.self_modulate.r8, 155, 0.01)
+		$health_indicator.modulate.r8 = lerp($health_indicator.modulate.r8, 155, 0.01)
