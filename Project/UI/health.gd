a847de3e89fa9f5f15c8694f3cb49e37d91aa706
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-06-23 10:49:16 +1200

    health bar animation

--- Project/UI/health.gd
+++ Project/UI/health.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var player = get_tree().get_first_node_in_group("player")
-var old_health: int
 
 func _process(_delta: float) -> void:
 	if not player:
@@ -13,3 +12,5 @@ func _process(_delta: float) -> void:
 		$health_indicator.modulate.r8 = 255
 	else:
 		$health_indicator.modulate.r8 = lerp($health_indicator.modulate.r8, 155, 0.01)
+
+	$health_indicator.frame = floor(player.health / 25)
