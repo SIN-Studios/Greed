0b490cfca92e2c29f0eb745de89c31a3e09fccb1
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-07-23 13:19:07 +1200

    inventroyb back to working after i broke it

--- Project/UI/Inventory/inventory_ui.gd
+++ Project/UI/Inventory/inventory_ui.gd
@@ -2,15 +2,16 @@ extends Control
 
 var is_open: bool
 @onready var inventory_slots: Array = $inventory/GridContainer.get_children()
-@export var player_inventory: inventory
+@export var player_inventory: inventory = preload("res://UI/Inventory/players_inventory.tres")
 
 
 func _ready() -> void:
 	close()
+	update_slots()
 
 func update_slots():
 	for i in range(min(player_inventory.items.size(), inventory_slots.size())):
-		inventory_slots[i].update(player_inventory.items[i])
+		inventory_slots[i].update_texture(player_inventory.items[i])
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_inventory"):
--- Project/UI/Inventory/inventory_ui_slot.gd
+++ Project/UI/Inventory/inventory_ui_slot.gd
@@ -3,8 +3,10 @@ extends Panel
 @onready var item_display: AnimatedSprite2D = $CenterContainer/Panel/item_display
 
 func update_texture(slot_item: enemy) -> void:
+	print("uh")
 	if !slot_item:
 		return
+	print("uh uh uh")
 	item_display.sprite_frames = slot_item.item_inventory_texture
 
 func update_item(slot_item: enemy) -> void:
