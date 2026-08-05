8eff9808d328d77576f4dcae041cf68c8017adbb
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-08-05 18:10:35 +1200

    fixed the bug that casued enemies without animations to not attack

--- Project/Enemies/States/enemy_attack_state.gd
+++ Project/Enemies/States/enemy_attack_state.gd
@@ -5,12 +5,12 @@ class_name EnemyAttackState
 func enter():
 	if control.get_node("AnimatedSprite2D").sprite_frames.has_animation("attack"):
 		control.get_node("AnimatedSprite2D").play("attack")
+		await control.get_node("AnimatedSprite2D").animation_finished
 	elif control.get_node("AnimatedSprite2D").sprite_frames.has_animation("default"):
 		control.get_node("AnimatedSprite2D").play("default")
 
-	await control.get_node("AnimatedSprite2D").animation_finished
 	SignalManager.player_take_damage.emit(15, (control.position.direction_to(Global.player.position)))
 	if control.get_node("AnimatedSprite2D").sprite_frames.has_animation("recoil"):
 		control.get_node("AnimatedSprite2D").play("recoil")
-	await control.get_node("AnimatedSprite2D").animation_finished
+		await control.get_node("AnimatedSprite2D").animation_finished
 	state_machine.change_state("enemychasestate")
