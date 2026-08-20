4d59d89008b9198d1d9a5297527e7f8ae3acd2f3
Isaac Choi <23332@burnside.school.nz>
2026-08-21 10:52:31 +1200

    lava and magma hurt over time and slow

--- Project/Player/States/player_hurt_state.gd
+++ Project/Player/States/player_hurt_state.gd
@@ -8,9 +8,9 @@ var direction: Vector2
 func take_damage(input_damage, input_direction):
 	damage = input_damage
 	direction = input_direction
+	state_machine.change_state("playerhurtstate")
 	SignalManager.shake_requested.emit(0.5) 
 	$"../../CPUParticles2D".restart()
-	state_machine.change_state("playerhurtstate")
 
 func enter():
 	control.modulate = Color8(255, 0, 0, 200)
--- Project/Player/States/player_walk_state.gd
+++ Project/Player/States/player_walk_state.gd
@@ -6,14 +6,16 @@ const speed: int = 75
 const slipperiness_factor: int = 5 #higher is more slippery
 
 func physics_update(_delta):
+
+	var current_speed = speed * control.speed_multiplier
 	var direction_x := Input.get_axis("move_left", "move_right")
 	var direction_y := Input.get_axis("move_up", "move_down")
 	if direction_x:
-		control.velocity.x = direction_x * speed
+		control.velocity.x = direction_x * current_speed
 	else:
 		control.velocity.x = move_toward(control.velocity.x, 0, slipperiness_factor)
 	if direction_y:
-		control.velocity.y = direction_y * speed
+		control.velocity.y = direction_y * current_speed
 	else:
 		control.velocity.y = move_toward(control.velocity.y, 0, slipperiness_factor)
 	control.move_and_slide()
