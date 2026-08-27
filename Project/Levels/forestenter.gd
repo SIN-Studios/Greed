65bc9710407c43da9cf945b2b7b95789889f56d3
Isaac Choi <23332@burnside.school.nz>
2026-08-27 20:45:24 +1200

    finished off area names

--- Project/Levels/forestenter.gd
+++ Project/Levels/forestenter.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		if body.global_position.x > global_position.x:
-			SignalManager.lava.emit()
+		if body.global_position.x < global_position.x:
+			SignalManager.forest.emit()
--- Project/UI/HUD/area_manager.gd
+++ Project/UI/HUD/area_manager.gd
@@ -1,4 +1,6 @@
 extends Control
+
+var active_tween: Tween
 @onready var lava = $Lava
 @onready var plains = $Plains
 @onready var forest = $Forest
@@ -12,16 +14,28 @@ func _ready() -> void:
 	plains.hide()
 	forest.hide()
 
+func show_area_text(target_node: Control) -> void:
+	if active_tween and active_tween.is_running():
+		active_tween.kill()
+	lava.hide()
+	plains.hide()
+	forest.hide()
+	target_node.show()
+	modulate.a = 0.0
+	active_tween = create_tween()
+	active_tween.tween_property(self, "modulate:a", 1.0, 1.0)
+	active_tween.tween_interval(3.6)
+	active_tween.tween_property(self, "modulate:a", 0.0, 1.0)
+	active_tween.tween_callback(target_node.hide)
 
-func lava_area_entered():
+func lava_area_entered() -> void:
 	print('lava')
-	lava.show()
-	
-func forest_entered():
+	show_area_text(lava)
+
+func forest_entered() -> void:
 	print('forest')
-	forest.show()
-	
-func plains_entered():
+	show_area_text(forest)
+
+func plains_entered() -> void:
 	print('plains')
-	plains.show()
-	
+	show_area_text(plains)
