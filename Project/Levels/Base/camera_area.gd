611ed9eef1d3efc9b47ca0f18ca7ec28152272a6
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-09-04 08:01:39 +1200

    added a global variable for if the player is in the base

--- Project/Levels/Base/camera_area.gd
+++ Project/Levels/Base/camera_area.gd
@@ -2,10 +2,12 @@ extends Area2D
 
 func _process(_delta: float) -> void:
 	if overlaps_body(Global.player):
+		Global.in_base = true
 		$"..".enabled = true
 		Global.player.get_node("Camera2D").enabled = false
 		Global.player.get_node("knife").visible = false
 	else:
+		Global.in_base = false
 		$"..".enabled = false
 		Global.player.get_node("Camera2D").enabled = true
 		Global.player.get_node("knife").visible = true
